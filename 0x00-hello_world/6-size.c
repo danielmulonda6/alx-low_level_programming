4e6e2a7edc3758e304fd96538fe40f79139a146d
danielmulonda6 <danielmulonda@outlook.com>
2023-02-20 04:02:30 -0800

    Adding 100-intel file.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,6 +5,13 @@
  * Return: Always 0(Success)
  */
 int main(void) {
+
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));

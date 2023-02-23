f47925b5f56c73f352402f318a4ddf20773c0f3f
danielmulonda6 <danielmulonda@outlook.com>
2023-02-22 18:33:27 -0800

    Adding 0-putchar.c file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; i <= 'z'; letter++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}

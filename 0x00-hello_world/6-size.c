975c9df761a31a6e815180fc4ab87b741b2635c2
danielmulonda6 <danielmulonda@outlook.com>
2023-02-24 15:26:59 -0800

    Adding 6-size.c file.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
 /**
- * main - A program that prints the size of various types on the computer it is compiled and run on.
+ * main - Entry point
  *
- * Return: Always 0(Success)
+ * Return: Always 0 (Success)
  */
-int main(void) {
-
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
-
+int main(void)
+{
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
